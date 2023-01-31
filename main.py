26565113de783c14c7fee77081636b6b86985383
chellick <matsupus@gmail.com>
2023-01-31 21:05:17 +0300

    main commit

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 from cvzone.PoseModule import PoseDetector
 import cv2
+from matplotlib import pyplot as pltd
+# cap = cv2.VideoCapture(0)
+# detector = PoseDetector()
+#
+# while True:
+#     success, img = cap.read()
+#     img = detector.findPose(img)
+#     lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+#     if bboxInfo:
+#         center = bboxInfo["center"]
+#         cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+#
+#     cv2.imshow("Image", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+import cv2
+import imutils
+
+# Initializing the HOG person
+# detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 cap = cv2.VideoCapture(0)
-detector = PoseDetector()
 
-while True:
-    success, img = cap.read()
-    img = detector.findPose(img)
-    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
-    if bboxInfo:
-        center = bboxInfo["center"]
-        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+while cap.isOpened():
+    # Reading the video stream
+    ret, image = cap.read()
+    if ret:
+        image = imutils.resize(image,
+                               width=min(400, image.shape[1]))
 
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Detecting all the regions
+        # in the Image that has a
+        # pedestrians inside it
+        (regions, _) = hog.detectMultiScale(image,
+                                            winStride=(4, 4),
+                                            padding=(4, 4),
+                                            scale=1.05)
+
+        # Drawing the regions in the
+        # Image
+        for (x, y, w, h) in regions:
+            cv2.rectangle(image, (x, y),
+                          (x + w, y + h),
+                          (255, 255, 255), 2)
+
+        # Showing the output Image
+        cv2.imshow("Image", image)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
         break
+
 cap.release()
 cv2.destroyAllWindows()
