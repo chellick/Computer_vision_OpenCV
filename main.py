664906f67256e445a1c052f8b71861bcc3586151
chellick <matsupus@gmail.com>
2023-01-31 20:48:25 +0300

    main commit

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
+from cvzone.PoseModule import PoseDetector
 import cv2
-import numpy as np
-def onlyThisOne(image, lower, upper, kernel):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv, lower, upper)
-    res = cv2.bitwise_and(image, image, mask=mask)
+cap = cv2.VideoCapture(0)
+detector = PoseDetector()
 
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+    if bboxInfo:
+        center = bboxInfo["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
 
-    return closing
-
-
-kernel = np.ones((5, 5), np.uint8)
-
-
-LOWER = np.array([0, 0, 200])
-UPPER = np.array([160, 160, 255])
-
-image = cv2.imread("images/WIN_20221219_16_35_22_Pro.jpg")
-image = cv2.resize(image, None, fx=0.5, fy=0.5)
-
-new_image = onlyThisOne(image, LOWER, UPPER, kernel)
-
-cv2.imshow("Yooohar", new_image)
-cv2.waitKey(0)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
