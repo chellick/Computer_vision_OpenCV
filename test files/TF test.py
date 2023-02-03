27715d298222a15bebbbf6f9a4b2d2819de351b6
chellick <matsupus@gmail.com>
2023-02-03 09:23:23 +0300

    np to tensor

--- test files/TF test.py
+++ test files/TF test.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models, optimizers
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+
+
+
 
 '''
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -41,10 +45,44 @@ print(np.array(a2))
 # tf.assign_add - добавиь
 # tf.assign_sub - убирать
 
-v1 = tf.Variable(-1.2)
-print(v1.assign_add(1))
+# v1 = tf.Variable(-1.2)
+# print(v1.assign_add(1))
+#
+#
+# v2 = tf.Variable([3, 1, 2, 5], dtype=tf.float32)
+# print(v2)
+#--------------------------------------------------------------------------------------------
 
 
-v2 = tf.Variable([3, 1, 2, 5], dtype=tf.float32)
-print(v2)
 
+image = cv2.imread("C:\python\GitHub\CV\images\WIN_20221219_16_35_22_Pro.jpg")
+
+
+
+
+def onlyThisOne(image, lower, upper, kernel):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower, upper)
+    res = cv2.bitwise_and(image, image, mask=mask)
+
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+
+    return closing
+
+
+kernel = np.ones((6, 6), np.uint8)
+
+
+LOWER = np.array([0, 0, 200])
+UPPER = np.array([160, 160, 255])
+
+new_image = onlyThisOne(image, LOWER, UPPER, kernel)
+
+
+
+tensor_image = tf.constant(new_image)
+
+
+print(tensor_image)
